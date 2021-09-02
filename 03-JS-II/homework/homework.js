0c59c168f880776487437273f030ee5464b1177e
Rshuespe <vigil.anam@hotmail.com>
2021-09-02 15:55:16 -0300

    homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -19,6 +19,7 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+   if()
 }
 
 function saludo(idioma) {
